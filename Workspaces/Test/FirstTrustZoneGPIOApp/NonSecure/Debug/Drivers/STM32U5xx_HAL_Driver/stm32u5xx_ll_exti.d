dbb9cfeefae04aa01dc9e963c5014f0bb725152d
marcosantoriello <santoriellomarco@yahoo.com>
2024-07-14 21:32:37 +0200

    Added first test project

---
+++ Workspaces/Test/FirstTrustZoneGPIOApp/NonSecure/Debug/Drivers/STM32U5xx_HAL_Driver/stm32u5xx_ll_exti.d
@@ -0,0 +1,2 @@
+Drivers/STM32U5xx_HAL_Driver/stm32u5xx_ll_exti.o: \
+ C:/Users/Marco/Desktop/Tesi/Tesi-STM32-TrustZone/Workspaces/Test/FirstTrustZoneGPIOApp/Drivers/STM32U5xx_HAL_Driver/Src/stm32u5xx_ll_exti.c
