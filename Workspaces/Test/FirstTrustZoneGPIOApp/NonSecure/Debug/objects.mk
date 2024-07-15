400e7a1df555226a01b05d51a870133961d6b95b
marcosantoriello <santoriellomarco@yahoo.com>
2024-07-15 14:44:50 +0200

    Added .gitignore

--- Workspaces/Test/FirstTrustZoneGPIOApp/NonSecure/Debug/objects.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (12.3.rel1)
-################################################################################
-
-USER_OBJS := C:/Users/Marco/Desktop/Tesi/Tesi-STM32-TrustZone/Workspaces/Test/FirstTrustZoneGPIOApp/Secure/Debug/secure_nsclib.o
-
-LIBS :=
-
